f2d8f7832e4c0708840c5a77dbbec3885f333186
Josh <nzryland@gmail.com>
2020-07-30 19:50:39 +1200

    fix background, add random button

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.getElementById("random");
+
+body.style.background = "linear-gradient(to right, " 
++ color1.value 
++ ", " 
++ color2.value 
++ ")";
+
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+  }  
 
 function setGradient() {
 	body.style.background = 
@@ -13,7 +29,19 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+function setRandomBackground(){
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ getRandomColor() 
+	+ ", " 
+	+ getRandomColor() 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("onClick", setRandomBackground);
